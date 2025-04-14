ba21da3b88584e604ca41c025c53996918d5d7ca
Anirudh Mishra <anirudhmishra112233@gmail.com>
2025-04-14 14:55:02 +0530

    cookies issue

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,15 @@
 import jwt from "jsonwebtoken";
 
-
 export const generateToken = (userId, res) => {
-    const token =jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d"
-    })
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,//MiliSec
-        httpOnly:true,//prevent XSS attacks cross-site scripting attacks
-        sameSite:"strict",//CSRF attacks cross-site request forgery attacks
-        secure:true
-    })
-    return token;
-}
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true, // ✅ required for HTTPS
+    sameSite: "None", // ✅ needed if cookies are sent cross-site
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+  return token;
+};
